92b55b31993e1455679893cea8d19badac6870b8
Žikica Drača <zikica.draca@gmail.com>
2023-12-12 17:40:28 +0100

    service cards correction

--- src/js/service.js
+++ src/js/service.js
@@ -79,10 +79,10 @@ const ourServicesCards = gsap.utils.toArray('.service-card-animation');
 
 ourServicesCards.forEach((card) => {
   gsap.from(card, {
-    scaleY: 1.3,
-    scaleX: 1.3,
+    // scaleY: 1.3,
+    // scaleX: 1.3,
     opacity: 0,
-    duration: 1.5,
+    duration: 1,
     // delay: 0.2,
     scrollTrigger: {
       trigger: card,
